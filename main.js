d925fd3ee0b284876b10c2a99ebdb93d1dea6b3d
AbdallahLearn <abdallahjhn1@gmail.com>
2024-09-15 15:54:21 +0300

    finsh lab4

---
+++ main.js
@@ -0,0 +1,20 @@
+function choose(phone){
+
+    let input = document.getElementById('input')
+    
+    if(phone ==1){
+        window.open(input.value ," ", "width=375, heigth=812")
+    }
+    else if(phone ==2){
+        window.open(input.value ," ", "width=412, heigth=869")
+    }
+    else if(phone ==3){
+        window.open(input.value ," ", "width=1024, heigth=1366")
+    }
+    else if(phone ==4){
+        window.open(input.value ," ", "width=1920, heigth=1080")
+    }
+}
+
+
+
